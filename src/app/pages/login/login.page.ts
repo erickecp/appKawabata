f6315e87ae60fa4ebce5e812e173274bec990b45
erickecp <43622490+erickecp@users.noreply.github.com>
2023-06-26 13:48:59 -0600

    minifix

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -31,10 +31,10 @@ export class LoginPage implements OnInit {
 
   generateForm() {
     this.formLogin = this.fb.group({
-      //userWeb: ['', Validators.required],
-       userWeb: ['KW23003A', Validators.required],
-      password: ['9900', Validators.required]
-     // password: ['', Validators.required]
+      userWeb: ['', Validators.required],
+       //userWeb: ['KW23003A', Validators.required],
+     // password: ['9900', Validators.required]
+      password: ['', Validators.required]
     });
   }
 
